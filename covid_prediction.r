a667efff2d24e8264d9a4a6019b25f90fa353fd1
ericbinnendyk <ericabq@gmail.com>
2020-10-31 22:35:02 -0600

    Add files via upload

--- covid_prediction.r
+++ covid_prediction.r
@@ -16,13 +16,13 @@ case.data = read.csv("source5/new_cases.csv", header=T)
 total_rows <- 278;
 total_cols <- 212;
 
-# find weekly cases from dataset
+# create dataset for predicting a certain number of weeks ahead
 # accumulate data for 37 weeks starting from January 19
-prepare.data <- function(case.data) {
+prepare.data <- function(case.data, num.weeks.ahead) {
   relevant.regions = c("World", "United.States", "China", "India", "Indonesia", "Pakistan", "Nigeria", "Brazil", "Bangladesh", "Russia", "Mexico");
   case.data = case.data[relevant.regions];
   week.data = read.table(text = "",
-                        col.names = relevant.regions);
+                         col.names = relevant.regions);
   total_week = rep(0, length(relevant.regions));
   i <- 21;
   while (i <= total_rows) {
@@ -35,23 +35,23 @@ prepare.data <- function(case.data) {
     }
     i = i + 1;
   }
-
+  
   # collect data from weeks 2 through 37
-  x = matrix(nrow = 36, ncol = 11);
+  x = matrix(nrow = 37 - num.weeks.ahead, ncol = 11);
   y = c();
-  i = 2;
+  i = 1 + num.weeks.ahead;
   # accumulate data for 39 weeks
   while (i <= 37) {
     # increase in world cases will be target feature
     target = week.data[i, 1];
-    target_as_boolean = (week.data[i, 1] > week.data[i - 1, 1]);
+    target_as_boolean = (week.data[i, 1] > week.data[i - num.weeks.ahead, 1]);
     # countries with the most cases (in previous week) can be our features
-    features = week.data[i - 1, ];
-    y[i - 1] = target_as_boolean;
-    x[i - 1, ] = as.numeric(features);
+    features = week.data[i - num.weeks.ahead, ];
+    y[i - num.weeks.ahead] = target_as_boolean;
+    x[i - num.weeks.ahead, ] = as.numeric(features);
     i = i + 1;
   }
-
+  
   data.frame(x, y = as.factor(y));
 }
 
@@ -69,12 +69,30 @@ kernel <- function(v1, v2) {
   total
 }
 
-dat = prepare.data(case.data);
+set.seed(50)
+dat = prepare.data(case.data, 2);
 num_instances = dim(dat)[1]
 train = sample(num_instances, replace=F, 15);
 train.data = dat[train, ]
 test.data = dat[-train, ]
 library(e1071)
 my.svm <- svm(y ~ ., data = train.data, cost = 10, scale = FALSE);
-predict(my.svm, train.data)
-predict(my.svm, test.data)
+train_accuracy <- mean(train.data$y == predict(my.svm, train.data))
+print(train_accuracy) # 1
+test_accuracy <- mean(test.data$y == predict(my.svm, test.data))
+print(test_accuracy) # 0.8571429 with seed 50
+library(caret)
+confusionMatrix(predict(my.svm, test.data), test.data$y)
+# test accuracy for 1 week:
+# 0.8571429
+# Confusion matrix for 1 week:
+# Prediction FALSE TRUE
+# FALSE     0    0
+# TRUE      3   18
+# test accuracy for 2 weeks:
+# 0.9
+# Confusion matrix for 2 weeks:
+# Reference
+# Prediction FALSE TRUE
+# FALSE     0    0
+# TRUE      2   18
